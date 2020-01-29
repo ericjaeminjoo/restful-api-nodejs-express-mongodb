41debe12b92f7b4368f0f669fd2616789545fbc9
Eric Jae-Min Joo <ericjaeminjoo@gmail.com>
2020-01-29 13:03:30 -0500

    Add connection to mongoose atlas with user credentials stored in .env file

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Connect to db
+mongoose.connect(
+  `${process.env.DB_CONNECTION}`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to DB!');
+  }
+);
 
 // Start listening to PORT
 app.listen(PORT, () => console.log(`Server is UP -> listening @ port ${PORT}`));
